d1a2b037f1acc909957975f0b58db844ebcb46b9
aniram112 <masyakmarina@gmail.com>
2021-10-27 18:25:25 +0300

    Initial Commit

---
+++ maroschupkina_1PW4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  maroschupkina_1PW4
+//
+//  Created by Marina Roshchupkina on 27.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
